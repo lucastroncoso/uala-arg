7be4cf97f1084049df1ee31b5b178fb5405b9e75
martin.sazzaroli <martin.sazzaroli@mediamonks.com>
2021-09-15 13:51:53 -0300

    Add content-wrapper global class

--- src/components/pages/MainPage/MainPage.tsx
+++ src/components/pages/MainPage/MainPage.tsx
@@ -3,7 +3,7 @@ import styles from './MainPage.module.scss';
 
 const MainPage: FunctionComponent = () => {
   return (
-    <main id={styles.main}>
+    <main id={styles.main} className="content-wrapper">
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
