ee4b3aa3835547246103b9a4eb4aaa725ecf41d0
martin.sazzaroli <martin.sazzaroli@mediamonks.com>
2021-09-20 14:40:08 -0300

    Use useGlobal in Hero as example

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,13 +1,30 @@
+import React, { useRef, useCallback } from 'react';
 import styles from './Hero.module.scss';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import HEROBG from '/public/hero_bkg.jpg';
+import BlockWrapper from '../BlockWrapper/BlockWrapper';
+import useGlobal from '../../store';
 
 const Hero = ({ content }) => {
+  const [{ locale }, { setLocale }] = useGlobal();
+
+  const onDownloadButtonClick = useCallback(() => {
+    setLocale('en');
+  }, [setLocale]);
+
   return (
-    <>
-      <h1>{content.title}</h1>
-      <p>{content.paragraph}</p>
-    </>
+    <section className={styles.hero}>
+      <div className={styles.background}>
+        <img src={HEROBG.src} />
+      </div>
+      <BlockWrapper>
+        <h1 className={styles.title}>{content.title}</h1>
+        <p className={styles.paragraph}>{content.paragraph}</p>
+        <button className={styles.button} onClick={onDownloadButtonClick}>
+          {content.buttonCopy}
+        </button>
+      </BlockWrapper>
+    </section>
   );
 };
 
--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,45 +1,33 @@
+import { useEffect } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/LandingPage.module.scss';
 import Hero from '../components/Hero/Hero';
-import ExampleComponent from '../components/Example/Example';
-import BlockWrapper from '../components/BlockWrapper/BlockWrapper';
-import classNames from 'classnames';
 import pageContent from '../data/SiteMXContent.json';
+import useGlobal from '../store';
 
 export default function MainPageAr() {
-  const locale = process.env.locale;
+  const [{ locale }, { setLocale }] = useGlobal();
 
-  console.log();
+  //Para rendear de nuevo la pagina cuando el estado global cambia desde un componente hijo tuve que agregar esto.
+  useEffect(() => {
+    setLocale(locale);
+  }, [locale]);
+
   return (
     <>
       <Head>
         <title>Uala Main Page AR</title>
         <meta name="description" content="Ualá" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <main className={styles.main}>
-        <BlockWrapper>
-          <Hero content={pageContent[locale].hero} />
-          <p className={styles.description}>
-            Get started by editing <code className={styles.code}>pages/LandingPage.js</code>
-          </p>
-          <ExampleComponent />
-        </BlockWrapper>
+      <main>
+        <Hero content={pageContent[locale].hero} />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </>
   );
 }
