b12f32595b58ac3078bd66ada9e83c292bd2131b
martin.sazzaroli <martin.sazzaroli@mediamonks.com>
2021-09-22 18:52:44 -0300

    Add VideoSection component

---
+++ components/VideoSection/VideoSection.js
@@ -0,0 +1,25 @@
+import React, { useRef, useCallback } from 'react';
+import styles from './VideoSection.module.scss';
+import PropTypes from 'prop-types';
+import BlockWrapper from '../BlockWrapper/BlockWrapper';
+import BackgroundSVG from '../../public/assets/svg/videosection-bg.svg';
+import PlayVideoButton from '../PlayVideoButton/PlayVideoButton';
+
+const VideoSection = () => {
+  return (
+    <section className={styles.section}>
+      <div className={styles.background}>
+        <BackgroundSVG />
+      </div>
+      <BlockWrapper className={[styles.videoWrapper]}>
+        <PlayVideoButton youtubeId={'tGtXlU1QE6k'} />
+      </BlockWrapper>
+    </section>
+  );
+};
+
+VideoSection.propTypes = {
+  content: PropTypes.object,
+};
+
+export default VideoSection;
