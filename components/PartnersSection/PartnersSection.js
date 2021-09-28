3d5df7db02112a0b66857c939e140628cf9d9875
martin.sazzaroli <martin.sazzaroli@mediamonks.com>
2021-09-28 10:50:39 -0300

    Add PartnerSection component

--- components/PartnersSection/PartnersSection.js
+++ components/PartnersSection/PartnersSection.js
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
 import styles from './PartnersSection.module.scss';
 import PropTypes from 'prop-types';
 import BlockWrapper from '../BlockWrapper/BlockWrapper';
@@ -6,12 +7,32 @@ import BlockWrapper from '../BlockWrapper/BlockWrapper';
 import Image from 'next/image';
 import MHorizontalCard from '../M-HorizontalCard/MHorizontalCard';
 import LOGO_PAYNET from '../../public/assets/images/paynet.png';
+import useIsMobile from '../utils/hooks/useIsMobile';
 
 const PartnersSection = ({ content }) => {
+  const isMobile = useIsMobile(768);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ skipSnaps: false });
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const scrollTo = useCallback((index) => emblaApi && emblaApi.scrollTo(index), [emblaApi]);
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return;
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, [emblaApi, setSelectedIndex]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+    onSelect();
+    setScrollSnaps(emblaApi.scrollSnapList());
+    emblaApi.on('select', onSelect);
+  }, [emblaApi, setScrollSnaps, onSelect]);
+
   return (
     <section className={styles.partnersSection}>
       <BlockWrapper>
-        <h1 className={styles.title}>{content.title}</h1>
+        <h3 className={styles.title}>{content.title}</h3>
         <div className={styles.buttonsWrapper}>
           {content.buttons.map((button) => (
             <a href={button.url} key={button.title} className={styles.buttonLink}>
@@ -26,9 +47,32 @@ const PartnersSection = ({ content }) => {
           <Image src={LOGO_PAYNET} width={156} height={74} />
           <p className={styles.partnersCopy}>{content.partnersCopy}</p>
           <div className={styles.logoGrid}>
-            {content.partnersLogos.map((logo) => {
-              return <img src={`/assets/images/partnersLogos/${logo}.png`} key={logo} alt={logo} />;
-            })}
+            <div className={styles.sliderContainer} ref={isMobile ? emblaRef : null}>
+              <div className={styles.sliderInner}>
+                {content.partnersLogos.map((logo) => {
+                  return (
+                    <img
+                      className={isMobile ? styles.carouselImg : ''}
+                      src={`/assets/images/partnersLogos/${logo}.png`}
+                      key={logo}
+                      alt={logo}
+                    />
+                  );
+                })}
+              </div>
+              {isMobile && (
+                <div className={styles.sliderDots}>
+                  {scrollSnaps.map((_, index) => (
+                    <button
+                      key={index}
+                      aria-label={`show logo number ${index}`}
+                      className={index === selectedIndex ? styles.selected : ''}
+                      onClick={() => scrollTo(index)}
+                    />
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
           <a href={content.viewMoreCTA.url} target="_blank" className={styles.viewMore}>
             {content.viewMoreCTA.copy}
