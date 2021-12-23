87244daccbab31c08d3886346619888f3e2d713b
lucastroncoso <lucastroncoso.seguros@gmail.com>
2021-12-23 10:20:13 -0300

    fix

--- pages/busquedas-laborales/index.jsx
+++ pages/busquedas-laborales/index.jsx
@@ -11,20 +11,23 @@ const axios = require('axios');
 export async function getStaticProps() {
     // add a try / catch loop for nicer error handling
     try {
-        const res = await axios(
-            'https://comeet.co/careers-api/2.0/company/54.00B/positions?token=45B15C715C78B6273322D88B6D111A22D11&details=true',
-            {
-                method: 'GET',
-                headers: {
-                    'content-type': 'application/json'
 
-                },
-                // throw our query (a string) into the body directly
 
-            },
-        );
-        const { data } = await res.json();
-        return { props: data };
+        var config = {
+            method: 'get',
+            url: 'https://www.comeet.co/careers-api/2.0/company/54.00B/positions?token=45B15C715C78B6273322D88B6D111A22D11&details=true',
+
+
+        };
+        const res = await axios(config)
+
+        const data = JSON.stringify(res.data)
+
+        return {
+            props: {
+                data
+            }
+        };
     } catch (error) {
         // add a descriptive error message first,
         // so we know which GraphQL query caused the issue
@@ -39,35 +42,28 @@ export async function getStaticProps() {
 
     }
 }
-//         .then(data => {
-
-//     var parseData = []
-//     data.forEach(element => {
-
-//         // if (element.location.country != "MX") {
-
-//         if (!parseData[element.department]) {
-//             parseData[element.department] = [];
-//         }
-
-//         parseData[element.department]["carrers"] ? parseData[element.department]["carrers"].push(element) : (parseData[element.department]["carrers"] = [element])
-//         parseData[element.department]["categoryTitle"] = element.department;
-
-//         // }
-
-//     });
-// });
-
-// return {
-//     props: { data }
-// }
-// }
-
-export default function Promociones(props) {
 
 
+export default function Promociones({ data }) {
+    data = JSON.parse(data);
+    let parseData = []
+    data.forEach(element => {
 
+        // if (element.location.country != "MX") {
 
+        if (!parseData[element.department]) {
+            parseData[element.department] = [];
+        }
+
+        parseData[element.department]["carrers"] ? parseData[element.department]["carrers"].push(element) : (parseData[element.department]["carrers"] = [element])
+        parseData[element.department]["categoryTitle"] = element.department;
+
+        // }
+
+    });
+    parseData.forEach(e => console.log(Object.keys(e)))
+    console.log(parseData)
+    // console.log(Object.keys(parseData[0]))
     return (
         <>
             <Head>
@@ -75,9 +71,20 @@ export default function Promociones(props) {
             </Head>
             <Layout nav footer banner>
 
+                <Container className="mt-48">
+
+                    {parseData.map(department => {
+                        <h2>{department}</h2>
+                    }
+                    )}
+                </Container>
 
             </Layout>
         </>
     )
+
+
+
+
 
 }
