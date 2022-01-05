4c29fe96ad585fd95afd17b6c19d9b1d657578bc
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-01-05 11:42:33 -0300

    Changed to display items in a column fashion

--- components/cardChildren.jsx
+++ components/cardChildren.jsx
@@ -2,9 +2,9 @@ import parser from 'bbcode-to-react';
 
 export default function CardChildren(props) {
     return (
-        <div className="bg-white rounded-3xl flex flex-wrap py-4 pl-10 pr-6 shadow-lightblue">
+        <div className="bg-white rounded-3xl py-4 pl-10 pr-6 shadow-lightblue">
             <img style={{ width: "90px", height: "90px" }} className="object-cover my-2 " src={props.img} alt="" />
-            <div className="min-h-full">
+            <div className="">
                 <h2 className="title-3 my-2 w-full self-start">{props.title}</h2>
                 {/* <p className="text lg:text-base mt-2 mb-4 ">{parser.toReact(props.text)}</p> */}
                 <div>{props.children}</div>
