1353353449edfca7a8e2a286af1fd8580d869599
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-01-05 15:06:43 -0300

    Revert "Now it accepts a noStretch property to choose between children taking full row height or not"
    This reverts commit 6e749423c410466ff5d80420865e41ed9c6d4d2a.

--- components/cardSection.jsx
+++ components/cardSection.jsx
@@ -5,7 +5,7 @@ import Container from "./container";
 export default function CardSection(props) {
     return (
         <Container className={props.className + " py-12"}>
-            <div className={`w-full grid lg:grid-cols-3 gap-8 gap-y-14 ${ !!props.noStretch ? 'items-start' : ''}`}>
+            <div className="w-full grid lg:grid-cols-3 gap-8 gap-y-14">
                 {props.children}
             </div>
         </Container>
