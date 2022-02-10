df2c0f7bfc5fef1ef6525a6fb2bbdd7653b9ce08
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-02-10 16:26:48 -0300

    Updated /costos page with new design

--- pages/costos.jsx
+++ pages/costos.jsx
@@ -1,68 +1,90 @@
-import Layout from "../components/layout";
-import Container from "../components/container";
+import { useState } from 'react';
+import Layout from '../components/layout';
+import Container from '../components/container';
 import Head from 'next/head';
-import Image from "next/image";
+import Image from 'next/image';
+import Hero from '../components/hero';
+import CardRoundedShadow from '../components/cardRoundedShadow';
+import { fetchContent } from '../utils/contentful';
+import CostosCard from '../components/costosCard';
 
 
-export default function Costos(props) {
+export async function getStaticProps() {
+  const response = await fetchContent(`
+  {
+    argentinaCostosCardsCollection (order: [order_ASC]) {
+      items {
+        order,
+        title,
+        description,
+        message
+      }
+    }
+  }     
+  `);
 
-    const charges = [
-        { cost: "$0", title: "Descargá la app" },
-        { cost: "$0", title: "Costo de apertura, mantenimiento y cierre" },
-        { cost: "$0", title: "Comisión por consumos con la tarjeta" },
-        { cost: "$0", title: "Transferencias entre usuarios Ualá " },
-        { cost: "$0", title: "Costo de envío" },
-        { cost: "$0", title: "Costo de renovación" },
-        { cost: "$0", title: "Cargas en efectivo", description: "Si cargás más de $90,000 en el mes, se cobrará 2,5% + IVA sobre el importe de carga excedido." },
-        { cost: "$0", title: "Carga por transferencia" },
-        { cost: "$0", title: "Costo por reposición", description: "la primera y 200ARS + IVA a partir de la segunda." },
-        { cost: "$105", title: "Retiros por cajero automático en la República Argentina", description: "+ IVA por extracción" },
-        { cost: "US$6", title: "Retiros por cajero automático en el exterior", description: "+ IVA cada una. Puede tener costo adicional del cajero. Por el momento esta opción se encuentra inhabilitada" },
-        { cost: "$0", title: "Retiros por canales alternativos (Rapipago)", description: "por mes y $75 + IVA a partir de la segunda" }
-    ]
+  return {
+    props: { response },
+    revalidate: 10
+  }
+}
 
-    return (
-        <>
-            <Head>
-                <title>Ualá</title>
-            </Head>
-            <Layout nav footer>
-                <Container className="md:mt-12 mt-24 bg-blue-wave-2 md:pb-44">
-                    <div className="grid lg:grid-cols-2 lg:mt-8 mb-26">
-                        <div className="flex content-center flex-wrap order-2">
-                            <div className="">
-                                <h1 className="title-1 ">
-                                    <div className="lg:text-5xl lg:leading-normal mb-12">No tenemos letra chica ni costos ocultos</div>
-                                </h1>
-                            </div>
-                        </div>
-                        <div className="flex justify-center order-1 " >
-                            <div className="lg:mt-16">
-                                <Image src="/assets/images/charge.gif" width={500} height={400} />
-                            </div>
-                        </div>
-                    </div>
-                </Container>
 
-                <div className="bg-blue-degrade mb-24 ">
-                    <Container>
-                        <div className="py-20 grid md:grid-cols-2 md:-mt-44">
-                            {
-                                charges.map(charge => (
-                                    <div className="grid">
-                                        <div className="bg-white rounded-md p-8 m-3 flex flex-col border-gray-5' rounded-2xl shadow-blue">
-                                            <div className="text-blue-600 text-center text-2xl">{charge.title}</div>
-                                            <div className="md:text-8xl text-5xl text-gray-800 text-center md:my-12 my-4 flex items-center justify-center">{charge.cost}</div>
-                                            {charge.description && <div className="flex-grow  flex items-center text-gray-600 text-center">{charge.description}</div>}
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </Container>
+export default function Costos2(props) {
+  const [data, setData] = useState(props.response.argentinaCostosCardsCollection.items);
+
+  return (
+    <>
+      <Head>
+        <title>Ualá</title>
+      </Head>
+      <Layout nav footer>
+        <div className="md:mt-12 bg-blue-costos">
+          <Hero
+            containerStyles="md:min-h-screen"
+            section="Costos"
+            title="Disfrutá sin costos ocultos"
+            subtitle="No tenemos letra chica ni costos ocultos."
+            img={<Image src="/assets/images/costos/hero-costos.jpg" width={1389} height={1134} />}
+          />
+          <div className="bg-costos">
+            <Container>
+            <div className="text-9xl text-blue-250 font-bold block md:hidden text-center mt-16 mb-6">$0</div>
+              <div className="grid md:grid-cols-3 pb-12 md:pb-20">
+                <div className="text-center md:text-left md:col-span-2 md:col-start-2 col-span-3">
+                    <p className="text-xl md:text-2xl text-blue-250">Descarga de la app</p>
+                    <p className="text-xl md:text-2xl text-blue-250">Costo de apertura, mantenimiento y cierre</p>
+                    <p className="text-xl md:text-2xl text-blue-250 -mb-1 md:mb-0">Comisión por consumos con la tarjeta</p>
                 </div>
-            </Layout>
-        </>
-    )
-
+                <div className="col-span-1 grid grid-cols-3 hidden md:block">
+                  <div className="col-span-1 row-span-3 hidden md:block text-9xl text-blue-250 text-right font-bold mr-10">$0</div>
+                </div>
+                <div className="col-span-2 col-start-2 grid grid-rows-3 hidden">
+                  <div className="my-12 row-start-2 text-xl md:text-2xl text-blue-250">Transferencias entre usuarios Ualá</div>
+                </div>
+                <div className="text-center md:text-left md:col-span-2 md:col-start-2 col-span-3">
+                    <p className="text-xl md:text-2xl text-blue-250">Transferencias entre usuarios Ualá</p>
+                    <p className="text-xl md:text-2xl text-blue-250 md:-mt-1">Costo de envío</p>
+                    <p className="text-xl md:text-2xl text-blue-250">Costo de renovación</p>
+                    <p className="text-xl md:text-2xl text-blue-250">Cargas en efectivo*</p>
+                    <p className="text-xl md:text-2xl text-blue-250">Carga por transferencia</p>
+                    <p className="text-xl md:text-2xl text-blue-250">Costo por reposición de tarjeta*</p>
+                </div>
+              </div>
+            </Container>
+          </div>
+        </div>
+        <Container>
+              <div className="py-8 md:py-16 grid md:grid-cols-3">
+                {
+                  data.length == 0 
+                    ? <></> 
+                    : data.map(dat => <CostosCard key={ dat.title } title={dat.title} subtitle={dat.description} message={dat.message} />) 
+                }
+              </div>
+              <div className="text-base text-gray-250 mb-20">*La primera reposición es gratuita. A partir de la segunda el valor es de 200ARS + IVA.</div>
+        </Container>
+      </Layout>
+    </>
+  );
 }
