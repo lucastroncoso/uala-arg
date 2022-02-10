0a466042b56906298051d6132b5341a00d458359
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-02-10 16:04:49 -0300

    Test deploy

---
+++ pages/compras2.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Tarjeta(props) {
+
+    return (
+        <>
+Test
+        </>
+    )
+
+}
---
+++ stories/pages/compras.stories.jsx
@@ -0,0 +1,8 @@
+import Compras from '../../pages/compras2.jsx';
+
+export default {
+    title: "Pages/Compras",
+    component: Compras
+};
+
+export const ComprasPage = () => <Compras />;
