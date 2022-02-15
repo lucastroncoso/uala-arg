87921b7750244771fb57be8b00182b81b8c6bac5
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-02-15 12:01:31 -0300

    Cleanup for Storybook and added default props and prop for shadow color

--- components/cardRoundedShadow.jsx
+++ components/cardRoundedShadow.jsx
@@ -1,8 +1,35 @@
-export default function CardRoundedShadow(props) {
+import PropTypes from 'prop-types';
+
+/**
+ * UI component for cards with rounded borders and shadow all around
+ */
+export default function CardRoundedShadow({shadowColor, roundXl, className, ...props}) {
 
     return (
-        <div className={`flex flex-col m-4 border border-gray-100 shadow-lightblue ${ props.roundXl ? 'rounded-3xl' : 'rounded-xl' } ${props.className} `}>
+        <div className={`flex flex-col m-4 border border-gray-100 shadow-${shadowColor} ${ roundXl ? 'rounded-3xl' : 'rounded-xl' } ${className} `}>
             { props.children }
         </div>
     )
 }
+
+
+CardRoundedShadow.propTypes = {
+    /**
+     * Is this card very rounded?
+     */
+    roundXl: PropTypes.bool,
+    /**
+     * What classes to use
+     */
+    className: PropTypes.string,
+     /**
+     * What color should the shadow be?
+     */
+    shadowColor: PropTypes.oneOf(['lightblue', 'blue']),
+};
+
+CardRoundedShadow.defaultProps = {
+    shadowColor: 'lightblue',
+    roundXl: false,
+};
+  
