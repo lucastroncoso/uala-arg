90aa345b6d2d5a1dfccd585c7cf1d9024f288b1d
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-02-15 16:45:43 -0300

    Cleanup for storybook and added propTypes

--- components/imageItemsSection.jsx
+++ components/imageItemsSection.jsx
@@ -1,30 +1,29 @@
+import PropTypes from 'prop-types';
 import Container from "./container";
 
-
-
-export default function ImageItemsSection(props) {
+export default function ImageItemsSection({bg, href, reverse, img, pushImg, title, subtitle, items, ...props}) {
     return (
-        <Container className={props.bg}>
-            <a target="_blank" href={props.href}>
+        <Container className={bg}>
+            <a target="_blank" href={href}>
                 <div className="grid md:my-24 my-12 lg:grid-cols-2 items-center gap-16 justify-items-center relative">
-                    <div className={props.reverse ? "order-2 rounded-3xl flex relative" : "order-2 lg:order-none rounded-3xl flex relative"}>
-                        <div className={props.reverse ? "order-2 rounded-3xl overflow-hidden flex" : "order-2 lg:order-none rounded-3xl overflow-hidden flex"}>
-                            {props.img}
+                    <div className={reverse ? "order-2 rounded-3xl flex relative" : "order-2 lg:order-none rounded-3xl flex relative"}>
+                        <div className={reverse ? "order-2 rounded-3xl overflow-hidden flex" : "order-2 lg:order-none rounded-3xl overflow-hidden flex"}>
+                            {img}
                         </div>
-                        {props.pushImg &&
+                        {pushImg &&
                             <div>
-                                <img src={props.pushImg} style={{ height: "112px" }} alt="" className=" lg:bottom-8 lg:-left-24 -bottom-4 hidden lg:flex absolute" />
-                                <img src={props.pushImg} style={{ height: "78px" }} alt="" className="absolute lg:bottom-8 lg:-left-24 -bottom-2 lg:hidden left-2/4 transform -translate-x-1/2 " />
+                                <img src={pushImg} style={{ height: "112px" }} alt="" className=" lg:bottom-8 lg:-left-24 -bottom-4 hidden lg:flex absolute" />
+                                <img src={pushImg} style={{ height: "78px" }} alt="" className="absolute lg:bottom-8 lg:-left-24 -bottom-2 lg:hidden left-2/4 transform -translate-x-1/2 " />
                             </div>
                         }
                     </div>
                     <div className="flex flex-col  order-1 ">
                         <div>
-                            {props.title && <div className="mb-4 title-2">{props.title}</div>}
-                            {props.subtitle && <div className="subtitle-2">{props.subtitle}</div>}
+                            {title && <div className="mb-4 title-2">{title}</div>}
+                            {subtitle && <div className="subtitle-2">{subtitle}</div>}
                         </div>
                         <div className="flex flex-col justify-around h-full lg:mr-12">
-                            {props.items && props.items.map((i) => i)}
+                            {items && items.map((i) => i)}
                         </div>
                     </div>
 
@@ -33,3 +32,38 @@ export default function ImageItemsSection(props) {
         </Container>
     );
 }
+
+ImageItemsSection.propTypes = {
+    /**
+     * Background style
+     */
+     bg: PropTypes.string,
+    /**
+     * Link URL
+     */
+     href: PropTypes.string,
+    /**
+     * Name of the page
+     */
+     title: PropTypes.string,
+    /**
+     * Subtitle
+     */
+     subtitle: PropTypes.string,
+    /**
+     * Image
+     */
+     img: PropTypes.element,
+     /**
+     * PushImage src
+     */
+      pushImg: PropTypes.string,
+    /**
+     * Are the columns reversed on mobile?
+     */
+     reverse: PropTypes.bool,
+    /**
+     * Items to show
+     */
+     items: PropTypes.arrayOf(PropTypes.element),
+};
