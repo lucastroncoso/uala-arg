6edf91d97e827ca465ac53b4fd89d960627dbea8
FlorBergesioU <florencia.bergesio@uala.com.ar>
2022-03-10 13:23:29 -0300

    Added story for StepsSection

---
+++ stories/StepsSection.stories.js
@@ -0,0 +1,37 @@
+import StepsSection from '../components/home/StepsSection/StepsSection';
+
+export default {
+    component: StepsSection,
+    title: 'StepsSection',
+};
+
+const Template = args => <div className="w-full"><StepsSection {...args} /></div>
+
+export const Default = Template.bind({});
+Default.args = {
+    content: {
+        title: "Cómo ser parte de Ualá",
+        stepsItems: [
+            {
+                copy: "Descargá la app desde Play Store o App Store en tu celular.",
+                icon: "pasos-envio-1",
+                index: 0
+            },
+            {
+                copy: "Completá el registro con tus datos desde la app y obtené tu tarjeta virtual.",
+                icon: "pasos-envio-2",
+                index: 1
+            },
+            {
+                copy: "Recibí tu tarjeta Ualá dentro de los 15 días hábiles, gratis y en todo el país.",
+                icon: "pasos-envio-3",
+                index: 2
+            },
+            {
+                copy: "¡Activá tu tarjeta desde la app y cargá saldo para empezar a usarla!",
+                icon: "pasos-envio-4",
+                index: 3
+            }
+        ]
+    }
+};
