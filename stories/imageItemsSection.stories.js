dfe94a990ca0221e1a7aacd97bd0d2e30c86ca69
Tomas Obregon <tomas.obregon@uala.com.ar>
2022-05-10 11:22:51 -0300

    imageitemsection ready

--- stories/imageItemsSection.stories.js
+++ stories/imageItemsSection.stories.js
@@ -1,5 +1,7 @@
 import ImageItemsSection from '../components/imageItemsSection';
 import CardItem from '../components/cardItem';
+import Button from "../components/StoryBookComps/Button-story";
+
 
 
 export default {
@@ -19,9 +21,8 @@ Default.args = {
         <CardItem text="Al registrarte contarás con una tarjeta digital para hacer compras online. "
             img="/assets/images/tarjeta/Icono compras.png" />,
         <CardItem text="Con chip, para que tu información esté más protegida que nunca."
-            img="/assets/images/tarjeta/Icono chip.png" />,
-        <CardItem text="Con tecnología contactless para que tus compras sean mucho más seguras."
-            img="/assets/images/tarjeta/Icono contactless.png" />]
+            img="/assets/images/tarjeta/Icono chip.png" />
+       ]
 }
 
 
@@ -35,13 +36,12 @@ Argentina.args = {
         <CardItem text="Al registrarte contarás con una tarjeta digital para hacer compras online. "
             img="/assets/images/tarjeta/Icono compras.png" />,
         <CardItem text="Con chip, para que tu información esté más protegida que nunca."
-            img="/assets/images/tarjeta/Icono chip.png" />,
-        <CardItem text="Con tecnología contactless para que tus compras sean mucho más seguras."
-            img="/assets/images/tarjeta/Icono contactless.png" />]
+            img="/assets/images/tarjeta/Icono chip.png" />]
 }
 
-export const MexicoUalabis = Template.bind({});
-MexicoUalabis.args = {
+export const ArgentinaReversed = Template.bind({});
+ArgentinaReversed.args = {
+  reverse: true,
     bg:"",
     title:"La tarjeta",
     subtitle:"Una tarjeta prepaga Mastercard internacional diseñada para tener la mejor experiencia de compra.",
@@ -50,7 +50,32 @@ MexicoUalabis.args = {
         <CardItem text="Al registrarte contarás con una tarjeta digital para hacer compras online. "
             img="/assets/images/tarjeta/Icono compras.png" />,
         <CardItem text="Con chip, para que tu información esté más protegida que nunca."
-            img="/assets/images/tarjeta/Icono chip.png" />,
-        <CardItem text="Con tecnología contactless para que tus compras sean mucho más seguras."
-            img="/assets/images/tarjeta/Icono contactless.png" />]
+            img="/assets/images/tarjeta/Icono chip.png" />]
 }
+
+export const MexicoUalabis = Template.bind({});
+MexicoUalabis.args = {
+   bg:"mb-20",
+    title:"Ofrece meses sin intereses a tus clientes",
+    subtitle:"Pueden ser de 3, 6, 9, 12 y 18 meses sin intereses.__¡Acepta todas las tarjetas!__",
+    img:(<img width={1389} height={1134} src="https://images.ctfassets.net/t5yal6u1wvnw/5c1p2zG4dggwCqY6jI2qH0/d022996ec5dc9a86bcc587141ab7fadb/Foto_MSI-Ajustada.webp" alt="my image" />),
+    items: [
+            <div className="w-6/12 mt-1">
+                    <img
+                src="https://images.ctfassets.net/t5yal6u1wvnw/38w8e5z64LcnzeCdpyWLuL/6e8f5cf76f3d2795ec1e6a64ca7e1125/Logos.png"
+                width={855}
+                height={180} />
+        
+            </div>
+            ,
+            <div className="mt-3 mb-8 md:mb-0 w-48">
+              <Button
+                href="https://onboarding.prod.adquirencia.mx.ua.la/?utm_source=web&utm_medium=ualabismx"
+                text="Empieza ahora"
+              /> 
+            </div>
+          ]
+}
+
+    
+       
