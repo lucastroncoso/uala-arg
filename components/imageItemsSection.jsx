f1b1d454fe3534f0d3906b4f214ca8c4079eafa7
Tomas Obregon <tomas.obregon@uala.com.ar>
2022-05-19 11:39:37 -0300

    animaciones in imageitemsection

--- components/imageItemsSection.jsx
+++ components/imageItemsSection.jsx
@@ -1,11 +1,62 @@
 import Container from './container';
+import useScrollTrigger from "../components/home/utils/hooks/useScrollTrigger"
+import gsap from 'gsap';
+import { useEffect } from 'react';
+
 
 export default function ImageItemsSection(props) {
+    const [sectionRef, childrenSelector, createTL] = useScrollTrigger();
+
+
+    useEffect(() => {
+    if (!sectionRef || !sectionRef.current) return;
+
+    const backgroundCards = childrenSelector('[data-animation="background-card"]');
+    const title = childrenSelector('[data-animation="title"]');
+    const titleTl = gsap.timeline({ paused: true });
+
+    const tl = createTL({
+      scrub: 1,
+      onEnter: () => titleTl.play(),
+      start: '0% 70%',
+    });
+
+    titleTl.fromTo(
+      title,
+      { scaleY: 0, transformOrigin: '0% 100%' },
+      { scaleY: 1, duration: 0.3, ease: 'power4.out' },
+      0,
+    );
+
+    const direction =  (index) => {
+      if (props.reverse) {
+ return   index % 2 ? -150 : 150
+      } else {
+         return   index % 2 ? 150 : -150
+      }
+     
+    }
+
+    backgroundCards.forEach((card, index) => {
+      gsap
+        .timeline({ scrollTrigger: { trigger: card, start: '0% 100%' } })
+        .fromTo(
+          card,
+          { x:direction(index) , transformOrigin: '50% 20%', opacity: 0 },
+          { duration: 0.65, x: 0, ease: 'Power4.InOut', opacity: 1 },
+        );
+    });
+  }, []);
+
   return (
+     <div ref={sectionRef}>
     <Container className={props.bg}>
       <a target="_blank" href={props.href}>
+        
         <div className="grid md:my-24 my-12 lg:grid-cols-2 items-center gap-16 justify-items-center relative">
-          <div
+          <div 
+                            data-animation="background-card"
+
             className={
               props.reverse
                 ? 'order-2 rounded-3xl flex relative'
@@ -38,7 +89,8 @@ export default function ImageItemsSection(props) {
               </div>
             )}
           </div>
-          <div className="flex flex-col  order-1 ">
+          <div                   data-animation="background-card"
+ className="flex flex-col  order-1 ">
             <div>
               {props.title && <h2 className="mb-4 title-2">{props.title}</h2>}
               {props.subtitle && <div className="subtitle-2">{props.subtitle}</div>}
@@ -64,5 +116,6 @@ export default function ImageItemsSection(props) {
         </div>
       </a>
     </Container>
+    </div>
   );
 }
