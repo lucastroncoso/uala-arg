11c68e64110298f94de9629b40507211ca136a64
Tomas Obregon <tomas.obregon@uala.com.ar>
2022-05-27 10:42:11 -0300

    finished anim hero and imageitemsection

--- components/hero.jsx
+++ components/hero.jsx
@@ -3,44 +3,25 @@ import parser from 'bbcode-to-react';
 import useScrollTrigger from "../components/home/utils/hooks/useScrollTrigger"
 import gsap from 'gsap';
 import { useEffect } from 'react';
-import useIsMobile from './home/utils/hooks/useIsMobile';
-
-
+import useIsMobile from './utils/hooks/useIsMobile';
 
 
 export default function Hero(props) {
-   const [sectionRef, childrenSelector, createTL] = useScrollTrigger();
-    const isMobile = useIsMobile(480) 
-
-
+     const [sectionRef, childrenSelector] = useScrollTrigger();
+    const isMobile = useIsMobile(768)
+   
     useEffect(() => {
     if (!sectionRef || !sectionRef.current) return;
 
     const animateItems = childrenSelector('[data-animation="animate-item"]');
-    const title = childrenSelector('[data-animation="title"]');
-    const titleTl = gsap.timeline({ paused: true });
-
-    const tl = createTL({
-      scrub: 1,
-      onEnter: () => titleTl.play(),
-      start: '0% 70%',
-    });
-
-    titleTl.fromTo(
-      title,
-      { scaleY: 0, transformOrigin: '0% 100%' },
-      { scaleY: 1, duration: 0.3, ease: 'power4.out' },
-      0,
-    );
-
+    
     const direction =  (index) => {
-      let number = isMobile?30:150
-      
+ let number = isMobile?30:150
       if (props.reverse) {
  return   index % 2 ? -number : number
       } else {
          return   index % 2 ? number : -number
-      }    
+      }   
     }
 
     animateItems.forEach((card, index) => {
